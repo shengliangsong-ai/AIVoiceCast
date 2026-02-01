cd3284d0c4c35977b0be09e1b06acbcfbc0cfd04
Sheng-Liang Song <shengliang.song@gmail.com>
2026-01-31 20:45:35 -0800

    Sync at Jan 31 8:42pm

---
+++ utils/content/mock_interview.ts
@@ -0,0 +1,6 @@
+
+import { SpotlightChannelData } from '../spotlightContent';
+
+export const MOCK_INTERVIEW_CONTENT: Record<string, SpotlightChannelData> = {
+  // mock-interview-deep-dive moved to judging.ts to resolve curriculum shadowing
+};
---
+++ utils/content/prism_platform.ts
@@ -0,0 +1,6 @@
+
+import { SpotlightChannelData } from '../spotlightContent';
+
+export const PRISM_PLATFORM_CONTENT: Record<string, SpotlightChannelData> = {
+  // judge-deep-dive moved to judging.ts to resolve curriculum shadowing
+};
